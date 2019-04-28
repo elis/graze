0e11f378223e170938ba631569b7a93106438282
Eli Sklar <eli.sklar@gmail.com>
2019-04-28 23:09:39 +0300

    cleanup

--- packages/razzle-plugin-mdx-front-matter/gray-matter-loader.js
+++ packages/razzle-plugin-mdx-front-matter/gray-matter-loader.js
@@ -2,7 +2,6 @@ const matter = require('gray-matter')
 const stringifyObject = require('stringify-object')
 
 module.exports = async function (src) {
-  console.log('Loading?', src)
   const callback = this.async()
   const { content, data } = matter(src)
   const code = `${content}
